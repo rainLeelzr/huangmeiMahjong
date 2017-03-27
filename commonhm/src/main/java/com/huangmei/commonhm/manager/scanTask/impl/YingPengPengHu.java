a0eb753c0a3d2d2fb623a2077412fe2ddf6500dd
rainLeelzr <529453975a@sina.com>
2017-03-27 15:10:07 +0800

    实现修改碰碰胡的set为list

--- commonhm/src/main/java/com/huangmei/commonhm/manager/scanTask/impl/YingPengPengHu.java
+++ commonhm/src/main/java/com/huangmei/commonhm/manager/scanTask/impl/YingPengPengHu.java
@@ -4,9 +4,7 @@ import com.huangmei.commonhm.manager.scanTask.abs.AbstractHuScanTask;
 import com.huangmei.commonhm.model.mahjong.Mahjong;
 import com.huangmei.commonhm.model.mahjong.PersonalCardInfo;
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 扫描是否硬碰碰胡
@@ -22,15 +20,17 @@ public class YingPengPengHu extends AbstractHuScanTask {
      */
     @Override
     public boolean doScan(PersonalCardInfo personalCardInfo) throws InstantiationException, IllegalAccessException {
-        Set<Mahjong> handCards = new HashSet<>(personalCardInfo.getHandCards());
+        log.debug("座位{}进行硬碰碰胡扫描！", personalCardInfo.getRoomMember().getSeat());
+
+        List<Mahjong> handCards = new ArrayList<>(personalCardInfo.getHandCards());
         handCards.add(putOutMahjong);
 
         // 按麻将的字号分组
-        Map<Integer, Set<Mahjong>> Mahjongs = groupByZiHao(handCards);
+        Map<Integer, List<Mahjong>> Mahjongs = groupByZiHao(handCards);
 
         // 判断每个字号组是否满足碰碰胡
         int[] elementSize = new int[]{1, 4, 7, 10, 13};
-        for (Set<Mahjong> mahjongs : Mahjongs.values()) {
+        for (List<Mahjong> mahjongs : Mahjongs.values()) {
             // 如果组内元素个数等于1, 4, 7, 10, 13,则肯定不是对对碰
             for (int size : elementSize) {
                 if (size == mahjongs.size()) {
@@ -39,10 +39,10 @@ public class YingPengPengHu extends AbstractHuScanTask {
             }
 
             // 按麻将牌的号码分组
-            Map<Integer, Set<Mahjong>> sameNumberMahjongs =
+            Map<Integer, List<Mahjong>> sameNumberMahjongs =
                     groupByNumber(mahjongs);
             int[] sameNumberMahjongElementSize = new int[]{1, 4};
-            for (Set<Mahjong> mahjongSet : sameNumberMahjongs.values()) {
+            for (List<Mahjong> mahjongSet : sameNumberMahjongs.values()) {
                 // 如果组内元素个数等于1, 4,则肯定不是对对碰
                 for (int size : sameNumberMahjongElementSize) {
                     if (size == mahjongSet.size()) {
@@ -64,7 +64,7 @@ public class YingPengPengHu extends AbstractHuScanTask {
      * 判断是否有重复的眼
      * sameMahjong只能是2或3个相同的麻将
      */
-    private boolean haveSecondEye(Set<Mahjong> sameMahjong) {
+    private boolean haveSecondEye(List<Mahjong> sameMahjong) {
         if (sameMahjong.size() == 2) {
             // 如果这个set的数量是2，说明是眼，再判断之前有没有眼
             if (hasEye) {
