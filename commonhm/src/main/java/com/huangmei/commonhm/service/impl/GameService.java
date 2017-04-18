6e48aa0acdf040eb4672327422aec94d6f43fda1
xuanjianbin <714936615@qq.com>
2017-04-18 14:34:56 +0800

    优化退出房间

--- commonhm/src/main/java/com/huangmei/commonhm/service/impl/GameService.java
+++ commonhm/src/main/java/com/huangmei/commonhm/service/impl/GameService.java
@@ -855,6 +855,8 @@ public class GameService {
             }
         } else {
             // 最后一局已结束，计算总结算
+            room.setState(Room.state.DISMISS.getCode());
+            roomDao.update(room);
             for (Score score : scores) {
                 roomService.outRoom(room.getRoomCode(), score.getUserId());
             }
